ebce7feb3967fcdd7150418fe930a7349deb773b
Devi <asldevi@gmail.com>
2013-06-15 07:57:46 +0530

    template loader to look in nodule templates also

--- nodules/__init__.py
+++ nodules/__init__.py
@@ -9,3 +9,17 @@ from ._version import *
 from nodules.models import *
 
 # Do not import content nodes until they are required by the client app
+
+
+# initialize app
+def init_app(app):
+    load_templates(app)
+
+def load_templates(app):
+    import jinja2, os.path
+    this_dir = os.path.abspath(os.path.dirname(__file__))
+    loader = jinja2.ChoiceLoader([
+                app.jinja_loader,
+                jinja2.FileSystemLoader([os.path.join(this_dir, 'templates')])
+            ])
+    app.jinja_loader = loader
