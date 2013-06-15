1484ea6c785301575b960e28839a24b4c7244564
Devi <asldevi@gmail.com>
2013-06-15 14:31:23 +0530

    init_app at each nodule to register respective nodetypes

--- nodules/__init__.py
+++ nodules/__init__.py
@@ -4,16 +4,33 @@ from __future__ import absolute_import
 
 from ._version import *
 
+from nodular import NodeRegistry, Node
+
 # Import basic nodes like User
 
-from nodules.models import *
+from nodules import page
+from nodules.models import User, db
 
 # Do not import content nodes until they are required by the client app
 
+registry = NodeRegistry()
+registry.register_node(Node)
+
+# fix this
+def get_root():
+    root = Node.query.filter_by(name='index').first()
+    if not root:
+        root = Node(name=u'index', title=u'Index Node')
+        db.session.add(root)
+        db.session.commit()
+    return root
 
 # initialize app
 def init_app(app):
     load_templates(app)
+    app.root_node = get_root()
+    # the `init_app` of respective nodules - should be configurable
+    page.init_app(app)
 
 def load_templates(app):
     import jinja2, os.path
--- nodules/page/__init__.py
+++ nodules/page/__init__.py
@@ -3,3 +3,11 @@
 from .models import *
 from .views import *
 from .forms import *
+
+
+def init_app(app):
+    from nodular import NodePublisher
+    import nodules
+
+    nodules.registry.register_node(PageType, view=PageView)
+    app.pagepub = NodePublisher(app.root_node, nodules.registry, u'/')
