641e8ab80d5b688df0db53972bd38f7cc5cae5d7
Devi <asldevi@gmail.com>
2013-06-15 14:29:06 +0530

    add bare templates to page and use them in view

--- nodules/page/views.py
+++ nodules/page/views.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
 
+from flask import render_template, request, redirect, url_for
+
 from nodular import NodeView
+from nodules.models import db
+
+from .forms import PageForm
+from .models import PageType
+
 
 class PageView(NodeView):
     @NodeView.route('/')
     def index(self):
-        return u'page index view'
+        return render_template('page/show.html', node=self.node)
 
     @NodeView.route('/edit', methods=['GET', 'POST'])
-    @NodeView.route('/', methods=['PUT'])
     @NodeView.requires_permission('edit', 'siteadmin')
     def edit(self):
-        return u'page edit view'
+        form = PageForm(request.form, self.node)
+        if form.validate_on_submit():
+            form.populate_obj(self.node)
+            db.session.commit()
+            return redirect(self.node.url_for('index'))
+        return render_template('page/edit.html', page=self.node, form=form)
 
     @NodeView.route('/delete', methods=['GET', 'POST'])
-    @NodeView.route('/', methods=['DELETE'])
     @NodeView.requires_permission('delete', 'siteadmin')
     def delete(self):
-        return u'page delete view'
+        if request.method == 'POST':
+            self.node.delete()
+        return redirect(self.node.url_for('index'))
