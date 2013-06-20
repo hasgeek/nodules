a7369ffc551742dbb9393150aa4f1d508483ff31
Devi <asldevi@gmail.com>
2013-06-20 18:17:30 +0530

    RichText in a better way, no need of patch to SqlAlchemy

--- nodules/models/richtext.py
+++ nodules/models/richtext.py
@@ -1,47 +1,48 @@
 # -*- coding: utf-8 -*-
 
-from flask import Markup
 from markdown import markdown
+
+from flask import Markup
+from sqlalchemy.ext.mutable import MutableComposite
 
 from nodules.models import db
 
 __all__ = ['RichTextColumn', 'RichText']
 
-class RichText(object):
-    def __init__(self, text, format=None):
+
+class RichText(MutableComposite):
+    def __init__(self, text, html=None, format=None):
+        self.format = format or self.getformat(text)
         self.text = text
-        self.format = format or self.getformat()
 
-        # store html only if the format is NOT HTML.
-        if self.format == 'text':
-            self.html = markdown(text)
-        else:
-            self.html = ''
-
-    @classmethod
-    def _from_db(cls, text, html, format):
-        if text is not None:
-            rt = RichText(text, format)
-            rt.html = Markup(html or text)
-            return rt
-        else:
-            return None
-
-    def getformat(self):
-        if self.text.startswith('<p>') and self.text.endswith('</p>'):
+    def getformat(self, text):
+        if text.startswith('<p>') and text.endswith('</p>'):
             return 'html'
         else:
             return 'text'
 
+    def __setattr__(self, key, value):
+        if key == 'text':
+            if self.format == 'text':
+                self._html = markdown(value)
+            else:
+                self._html = ''
+        object.__setattr__(self, key, value)
+        self.changed()
+
     def __composite_values__(self):
-        return (self.text, self.html, self.format)
+        return (self.text, self._html, self.format)
 
     def __str__(self):
         return self.text
 
+    @property
+    def html(self):
+        return Markup(self._html or self.text)
+
 
 def RichTextColumn(db, col_name):
-    return db.composite(RichText._from_db,
+    return db.composite(RichText,
              db.Column(col_name + '_text', db.UnicodeText),
              db.Column(col_name + '_html', db.UnicodeText),
              db.Column(col_name + '_format', db.Unicode(20)))
