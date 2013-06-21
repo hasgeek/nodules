7b0b8f8a155e4f44110da64aab490a89739fc4c1
Devi <asldevi@gmail.com>
2013-06-21 14:47:47 +0530

    allow page to be a child

--- nodules/page/__init__.py
+++ nodules/page/__init__.py
@@ -5,12 +5,12 @@ from .views import *
 from .forms import *
 
 
-def init_nodule(root, registry, pages_base_path='/'):
+def init_nodule(root, registry, base_path='/'):
     """
     Publish the nodule and return the publisher
     """
     from nodular import NodePublisher
 
-    registry.register_node(Page, view=PageView)
-    pagepub = NodePublisher(root, registry, pages_base_path)
+    registry.register_node(Page, view=PageView, parent_nodetypes=['*'])
+    pagepub = NodePublisher(root, registry, base_path)
     return pagepub
