fd6e854e6485a880c6e7d60f9a6fdcf9ffe89dba
Devi <asldevi@gmail.com>
2013-06-22 17:56:00 +0530

    look for child with name 'index' in folder view '/'

--- nodules/folder/views.py
+++ nodules/folder/views.py
@@ -5,12 +5,17 @@ from flask import render_template, request, redirect, flash
 from nodular import NodeView, Node
 from nodules import db, registry
 
+
 class FolderView(NodeView):
-    # to be improved
-    @NodeView.route('/index')
+    @NodeView.route('/blah')
     def index(self):
-        children = Node.query.filter_by(parent=self.node)
-        return render_template('folder/index.html', children=children)
+        index_node = Node.query.filter_by(parent=self.node, name='index').first()
+        if index_node:
+            # fix me: this shouldn't be redirect
+            return redirect(index_node.path)
+        else:
+            children = Node.query.filter_by(parent=self.node)
+            return render_template('folder/index.html', children=children)
 
     @NodeView.route('/delete', methods=['GET', 'POST'])
     @NodeView.requires_permission('delete', 'siteadmin')
