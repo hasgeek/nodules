245157324e8aa403fdc4d9345889fe2f220b8c32
Devi <asldevi@gmail.com>
2013-06-22 18:09:40 +0530

    fix index of folder

--- nodules/folder/views.py
+++ nodules/folder/views.py
@@ -11,8 +11,8 @@ class FolderView(NodeView):
     def index(self):
         index_node = Node.query.filter_by(parent=self.node, name='index').first()
         if index_node:
-            # fix me: this shouldn't be redirect
-            return redirect(index_node.path)
+            view_cls = registry.nodeviews.get(index_node.type)[0]
+            return view_cls(index_node).show()
         else:
             children = Node.query.filter_by(parent=self.node)
             return render_template('folder/index.html', children=children)
