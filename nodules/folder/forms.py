8ee92ca6b23afb4cad6b08ae29f49f13f2bad0b4
Devi <asldevi@gmail.com>
2013-07-16 17:50:20 +0530

    add new folder view

--- nodules/folder/forms.py
+++ nodules/folder/forms.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+from flask.ext.wtf import Form, TextField, Required
+
+# @@TODO
+def existing_folder(form, field):
+    pass
+
+
+class NewFolderForm(Form):
+    name = TextField('URL name', validators=[Required()])
+    title = TextField('Title', validators=[Required()])
--- nodules/folder/views.py
+++ nodules/folder/views.py
@@ -5,6 +5,8 @@ from flask import render_template, request, redirect, flash
 from nodular import NodeView, Node
 from nodules import db, registry
 
+from .models import Folder
+from .forms import NewFolderForm
 
 class FolderView(NodeView):
     @NodeView.route('/')
@@ -16,3 +18,17 @@ class FolderView(NodeView):
         else:
             children = Node.query.filter_by(parent=self.node)
             return render_template('folder/index.html', children=children)
+
+
+class NewFolderView(NodeView):
+    @NodeView.route('/new/folder', methods=['GET', 'POST'])
+    def index(self):
+        nf = NewFolderForm(request.form)
+        if nf.validate_on_submit():
+            f = Folder(name=nf.name.data, title=nf.title.data, parent=self.node)
+            nf.populate_obj(f)
+            db.session.commit()
+            flash('Folder added with title %s.' % nf.title.data)
+            return redirect('/')
+        folders = Folder.query.all()
+        return render_template('folder/newfolder.html', form=nf, folders=folders)
