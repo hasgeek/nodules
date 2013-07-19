c26a4a88cdb72fa5db37e1a2436b83e8bac673fb
Devi <asldevi@gmail.com>
2013-07-19 15:56:27 +0530

    add folders within folders. aha.

--- nodules/folder/__init__.py
+++ nodules/folder/__init__.py
@@ -13,5 +13,6 @@ def init_nodule(root, registry, base_path='/', urlpath=None):
     from nodular import NodePublisher
     registry.register_node(Folder, view=FolderView, child_nodetypes=['*'], parent_nodetypes=['*'])
     registry.register_view(Folder, NewPageView)
+    registry.register_view(Folder, NewFolderView)
     folder_pub = NodePublisher(root, registry, base_path, urlpath)
     return folder_pub
