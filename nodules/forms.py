15ae31b4c0edceebe03575dfc4acbd645efe8ca4
Devi <asldevi@gmail.com>
2013-08-05 15:51:45 +0530

    strip trailing commas in tags

--- nodules/forms.py
+++ nodules/forms.py
@@ -33,7 +33,7 @@ class TagsField(TextField):
     def process_formdata(self, data):
         # csv to list of tag objects
         if data[0]:
-            tag_titles = (t.strip() for t in data[0].strip().split(','))
+            tag_titles = (t.strip() for t in data[0].strip(' ,').split(','))
             self.data = utils.get_or_make_tags(tag_titles)
         else:
             self.data = []
